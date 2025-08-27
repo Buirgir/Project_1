32c1e4f13e3b8e51c8dcd53e7dbb38cf61cee8fe
Buirgir <viktor@lindhardsen.org>
2025-08-27 10:41:26 +0200

    First Commit

---
+++ Madlibs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System.Numerics;
+
+string name;
+string age;
+string personnummer;
+
+// name = "Viktor" + " Hjalmar" + " Lindhardsen";
+Console.WriteLine("Vad heter du");
+name = Console.ReadLine();
+
+Console.WriteLine("Hur gammal är du");
+age = Console.ReadLine();
+
+Console.WriteLine("Vad är ditt personnummer");
+personnummer = Console.ReadLine();
+
+Console.WriteLine($"Tack för att du gav den Chinesiska Lederskapet denna informationen: Ditt namn är {name}, Du är {age} år gtammal och ditt personnummer är {personnummer} ");
+Console.ReadLine();
